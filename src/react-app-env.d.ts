07f1b5528e23e4410ec7b74e020f333e929d3500
Pedro-Pomponet <pomponetpedro15@gmail.com>
2025-05-14 16:19:24 -0300

    Ajustes: Corrigido tipagem de imagens e removido pastas desnecessárias

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,26 @@
 /// <reference types="react-scripts" />
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.PNG' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
